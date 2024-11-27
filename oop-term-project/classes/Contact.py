29fa5a02d1a548068d4b87ad1531681e518a41dd
moldyharriet <chasedgibbs@proton.me>
2024-11-27 15:43:28 -0600

    cleaned up Contact.py, preparing to restart on Event.py to clean it up as well

--- oop-term-project/classes/Contact.py
+++ oop-term-project/classes/Contact.py
@@ -1,67 +1,18 @@
 import json
 
-
 class Contact:
 
     #constructor to pull data in from json dictionary
     def __init__(self, d: dict):
-        #there is most definitely a cleaner way to implement this, but i'm unsure the level of creative freedom we're allowed
-
-        #UID
-        if(d.get("UID") == ""):
-            self.__uid == "-1"
-        else:
-            self.__uid = d.get("UID");
-        
-        #firstname
-        if(d.get("FirstName") == ""):
-            self.__firstname = "--"
-        else:
-            self.__firstname = d.get("FirstName");
-
-        #lastname
-        if(d.get("LastName") == ""):
-            self.__lastname = "--"
-        else:
-            self.__lastname = d.get("LastName");
-
-        #email
-        if(d.get("EmailAddress") == ""):
-            self.__lastname = "--"
-        else:
-            self.__email = d.get("EmailAddress");
-
-        #department
-        if(d.get("Dept") == ""):
-            self.__lastname = "--"
-        else:
-            self.__department = d.get("Dept");
-        
-        #title
-        if(d.get("Title") == ""):
-            self.__lastname = "--"
-        else:
-            self.__title = d.get("Title");
-        
-        #phone
-        if(d.get("Phone") == ""):
-            self.__lastname = "--"
-        else:
-            self.__phone = d.get("Phone");
-        
-        #building
-        if(d.get("Building") == ""):
-            self.__lastname = "--"
-        else:
-            self.__building = d.get("Building");
-        
-        #mail_code
-        if(d.get("POBox") == ""):
-            self.__lastname = "--"
-        else:
-            self.__mail_code = d.get("POBox");
-
-        #last_contact
+        self.__firstname = d.get("FirstName", "--");
+        self.__lastname = d.get("LastName", "--");
+        self.__uid = d.get("UID", "-1");
+        self.__email = d.get("EmailAddress", "--");
+        self.__department = d.get("Dept", "--");
+        self.__title = d.get("Title", "--");
+        self.__phone = d.get("Phone", "--");
+        self.__building = d.get("Building", "--");
+        self.__mail_code = d.get("POBox", "--");
         self.__last_contact = "";
 
     def __str__(self) -> str:
@@ -89,48 +40,44 @@ class Contact:
             self.__mail_code == other.mail_code and \
             self.__last_contact == other.last_contact
     
-    """begin getters"""
-    @property
-    def uid(self):
-        return self.__uid;
-    
+    #begin getters
     @property
     def firstname(self):
         return self.__firstname;
 
     @property
     def lastname(self):
-        return self.__last_name;
-    
+        return self.__lastname;
+
+    @property
+    def uid(self):
+        return self.__uid;
+
     @property
     def email(self):
-        return self.__email;
+        return self.__email
 
     @property
     def department(self):
         return self.__department;
-    
+
     @property
     def title(self):
         return self.__title;
-    
+
     @property
     def phone(self):
         return self.__phone;
-    
+
     @property
     def building(self):
         return self.__building;
-    
+
     @property
     def mail_code(self):
         return self.__mail_code;
-    
-    @property
-    def last_contact(self):
-        return self.__last_contact;
 
-    """setter for last_contact var"""
+    #setter for last_contact
     @property
-    def last_contact(self, last_contact:str):
+    def last_contact(self, last_contact: str):
         self.__last_contact = last_contact;
